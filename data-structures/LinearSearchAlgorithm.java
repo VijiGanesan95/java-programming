d650fa320568075ede7476fd823d598e1f95d77f
VijiGanesan95 <124471022+VijiGanesan95@users.noreply.github.com>
2023-02-05 04:47:22 +0530

    Update LinearSearchAlgorithm.java

--- data-structures/LinearSearchAlgorithm.java
+++ data-structures/LinearSearchAlgorithm.java
@@ -1,4 +1,4 @@
-package com.myprogram.datastructures.data_structures;
+package data-structures;
 import java.util.*;
 public class LinearSearchAlgorithm {//Time complexity - O(n)
 	public static void main(String args[]) {
