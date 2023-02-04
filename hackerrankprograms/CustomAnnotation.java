570ff2703dc7598176a7cea609d16985167b53a8
VijiGanesan95 <124471022+VijiGanesan95@users.noreply.github.com>
2023-02-05 04:54:29 +0530

    Delete hackerrankprograms directory

--- hackerrankprograms/CustomAnnotation.java
+++
@@ -1,9 +0,0 @@
-package hackerrankprograms;
-
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
-public @interface CustomAnnotation {
-	int value();
-}
